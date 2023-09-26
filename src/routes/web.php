2dcd10cfe47f3b29c96c0d7716f3b2e9c95b489b
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-09-26 08:39:34 +0100

    fixing issues

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
 use tauseedzaman\ComingSoon\Http\Controllers\Admin\ComingSoonController;
-use Illuminate\Routing\Route;
 
-Route::prefix("/admin")->name("coming_soon.")->group(function () {
-    Route::get("/coming-soon", [ComingSoonController::class, "index"])->name("index");
-    Route::get("/coming-soon/create", [ComingSoonController::class, "create"])->name("create");
-    Route::post("/coming-soon/store", [ComingSoonController::class, "store"])->name("store");
-    Route::get("/coming-soon/edit/{id}", [ComingSoonController::class, "edit"])->name("edit");
-    Route::post("/coming-soon/edit/{id}", [ComingSoonController::class, "update"])->name("update");
-    Route::get("/coming-soon/delete/{id}", [ComingSoonController::class, "destroy"])->name("delete");
-});
+Route::get("/admin/coming-soon", [ComingSoonController::class, "index"])->name("coming_soon.index");
+Route::get("/admin/coming-soon/create", [ComingSoonController::class, "create"])->name("coming_soon.create");
+Route::post("/admin/coming-soon/store", [ComingSoonController::class, "store"])->name("coming_soon.store");
+Route::get("/admin/coming-soon/edit/{id}", [ComingSoonController::class, "edit"])->name("coming_soon.edit");
+Route::post("/admin/coming-soon/edit/{id}", [ComingSoonController::class, "update"])->name("coming_soon.update");
+Route::get("/admin/coming-soon/delete/{id}", [ComingSoonController::class, "destroy"])->name("coming_soon.delete");
