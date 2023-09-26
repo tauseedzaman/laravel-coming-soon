27c3a0b3c38007931d834237ec466d4d84d7facf
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-09-26 08:36:03 +0100

    fixing issues

--- src/ComingSoonServiceProvider.php
+++ src/ComingSoonServiceProvider.php
@@ -3,15 +3,14 @@
 namespace tauseedzaman\ComingSoon;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Console\AboutCommand;
 
 class ComingSoonServiceProvider extends ServiceProvider
 {
     public function boot()
     {
 
-        // $this->publishes([
-        //     __DIR__.'/views' => base_path('resources/views/comingsoon'),
-        // ]);
+        AboutCommand::add('Laravel Coming Soon', fn () => ['Version' => '1.0.0']);
 
         $this->mergeConfigFrom(
             __DIR__ . '/config/Comingsoon.php',
@@ -23,18 +22,30 @@ class ComingSoonServiceProvider extends ServiceProvider
         ]);
 
         $this->publishes([
-            __DIR__ . '/assets/js' => base_path('public/js'),
+            __DIR__ . '/assets/js' => base_path('public/laravel_comming_soon/js'),
         ]);
 
         $this->publishes([
-            __DIR__ . '/assets/css' => base_path('public/css'),
+            __DIR__ . '/assets/admin' => base_path('public/laravel_comming_soon/admin'),
         ]);
 
-        $this->loadViewsFrom(__DIR__ . '/views', 'comingsoon');
+        $this->publishes([
+            __DIR__ . '/assets/css' => base_path('public/laravel_comming_soon/css'),
+        ]);
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'Comingsoon');
+
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/laravel_coming_soon'),
+        ]);
 
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        $this->publishes([
+            __DIR__ . '/database/migrations/' => database_path('migrations')
+        ], 'laravel_coming_soon');
     }
 
     public function register()
